fff49a51cf988c17fa2741f4ff02d46ad936d30f
Roger Howard <rogerhoward@mac.com>
2018-09-27 09:28:10 -0400

    Ready for initial release

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# digipy
+# dzpy
+Digizuite Python client implementations
+
+## Importing Tree Files
+The first module in this package, "treeimport", takes a data file representing a hierarchy and imports it into an existing tree field in a Digizuite solution.
+
+### The data file format
+The file format required for this tool is very simple and demonstrated with an example in the samples/ directory. The text file was generated directly out of the corresponding Excel file; the assumption is that Excel is used to prepare the hierarchy definition, and then simply copying all the relevant rows and pasting into a text file creates the appropriate format.
+
+Alternately you can create this file directly using whatever tool you're comfortable with:
+
+* Each node is a line in the file
+* the nesting of each node is defined by how many tabs it has at the beginning of the line. 
+* There can be no empty rows
+* If a node already exists it will generate an error
+
+## Common usage
+This is a command line application - currently built for Windows, but is easily run on Mac OSX or Linux as well.
+
+The most common usage (using a Windows example) would look like:
+
+	treeimport.exe --file="import.txt" --field=50751 --baseurl="http://dc.acme.com" --username="System" --password="P@ssW0rD"
+
+All five parameters are required and should be set according to these guidelines:
+
+* file: this is a path (relative or absolute) to the import data file
+* field: this is the unique field ID of a Tree field in your Digizuite solution
+* baseurl: this is the base URL to your Digizuite solution
+* username: this defaults to "System" so use that unless you know better!
+* password: this is the actual password for the provided username
+
+## Questions? 
+
+I'll add them to an FAQ here.
---
+++ dz.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+import json
+import uuid
+import requests
+from urllib.parse import urljoin
+
+
+class Client(object):
+    BASE_URL = None
+    USER_NAME = None
+    PASSWORD = None
+
+    def __init__(self, url, username, password):
+        self.BASE_URL = url
+        self.USER_NAME = username
+        self.PASSWORD = password
+
+    def importTermFile(self, file, fieldID):
+        login = self.GetConnectionAccessKey()
+        if login is False:
+            return False
+
+        with open(file) as tree:
+            parentNodes = [0, ]
+            latestNode = 0
+            lastLevel = 1
+
+            for node in tree:
+                level = node.rstrip().count('\t') + 1
+                term = node.strip()
+                print(term, level)
+
+                if level > lastLevel:
+                    # The last node processed is this node's parent, 
+                    # add it to the stack
+                    parentNodes.append(latestNode)
+                elif level < lastLevel:
+                    # Pop one or more parent nodes off the stack
+                    # to match the current depth
+                    [parentNodes.pop() for _ in range(lastLevel - level)]
+
+                # Create a new term
+                latestNode = self.createTerm(fieldID, term, parent=parentNodes[-1], accesskey=login)
+                lastLevel = level
+
+
+    def GetConnectionAccessKey(self):
+        url = urljoin(self.BASE_URL, 'dmm3bwsv3/ConnectService.js')
+        method ='LogOn'
+
+        payload = {'method': method, 'username': self.USER_NAME, 'password': self.PASSWORD}
+        r = requests.request("POST", url, data=payload)
+
+        res = r.json()
+        if res['success'] == 'true':
+            return res['items'][0]['accessKey']
+        else:
+            return False
+
+
+    def createTerm(self, field, term, parent=[0], accesskey=None):
+        url = urljoin(self.BASE_URL, 'apiproxy/BatchUpdateService.js')
+
+        # If caller didn't pass in an access key, try to get one
+        if accesskey is None:
+            accesskey = self.GetConnectionAccessKey()
+            if accesskey is False:
+                return False
+
+        # Read in basic structure of "values" payload
+        with open('templates/values.json') as json_file:
+            payload_values = json.load(json_file)
+
+        # Insert custom values into "values" payload
+        payload_values[0]['Values'].append({"FieldId": "item_metafield_labelid", "Type": 3, "Values": field})
+        payload_values[0]['Values'].append({"FieldId": "prevref", "Type": 3, "Values": parent})
+        payload_values[0]['Values'].append({"FieldId": "treevalue", "Type": 1, "Values": [term]})
+        payload_values[0]['Values'].append({"FieldId": "optionvalue", "Type": 1, "Values": [str(uuid.uuid4())]})
+
+        # Read in XML contents for "updateXML" payload
+        with open("templates/update.xml") as xml_file:  
+            payload_xml = xml_file.read()
+
+        # Create requests payload dictionary
+        payload = {'updateXML': payload_xml, 'values': json.dumps(payload_values)}
+
+        # Make POST request, passing accesskey as a query param and other payload as form encoded
+        r = requests.request("POST", url, data=payload, params={'accesskey':accesskey})
+        res = r.json()
+
+        # Return creeated term's ID if successful
+        if res['success']:
+            return res['items'][0]['BaseId']
+        else:
+            return False
+
+
