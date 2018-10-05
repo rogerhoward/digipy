71cb81ffb67fb79953bab12c7e0fca6597a212c6
Roger Howard <rogerhoward@mac.com>
2018-10-05 07:53:41 -0400

    Improve

--- populatesearches.py
+++ populatesearches.py
@@ -3,12 +3,20 @@
 import dz
 import click
 from urllib.parse import urljoin
+import requests
 
+products = {'DC': '3AD2C82D-EE80-40CD-A404-8D1AD124639D',
+            'MM': 'F10ABF14-6FB1-4515-A16A-0C6C02376989',
+            'ACCC': '2350F493-D4AB-48AE-AC07-562242104035',
+            'MSOC': '726D5F54-C5FF-4F78-B931-167DAAAE389B',
+            'DFS': 'AC045BF0-C538-4397-BC13-EF6A61DF6A82',
+            'VP': 'F77A0B88-F80A-45CE-A5B9-65B6E7817FBE',
+            }
+product_choices = products.keys()
 
-def mode_switch(ctx, param, value):
-    print(ctx, param, value)
-
-
+def getEndpoint(url, params):
+    r = requests.request("GET", url, params=params)
+    return r.status_code, r.text
 
 
 
@@ -19,34 +27,69 @@ def mode_switch(ctx, param, value):
 @click.option('--password', required=True, prompt='Please enter the API user password', help='API password.')
 def cli(ctx, baseurl, username, password):
     dzClient = dz.Client(baseurl, username, password)
-
     ctx.ensure_object(dict)
     ctx.obj['client'] = dzClient
 
 
 @cli.command()
 @click.pass_context
-def GenerateAndPopulateAllSearches(ctx):
+def regenall(ctx):
     """
-    GenerateAndPopulateAllSearches is...
+    Regenerate and repopulate all searches in all products.
     """
     c = ctx.obj['client']
 
-    template = '/apiproxy/JobService.js?accesskey={apikey}&method=GenerateAndPopulateAllSearches'.format({'apikey': c.apikey})
-    print(template)
-    print('GenerateAndPopulateAllSearches is running', c.BASE_URL)
+    # apikey = c.GetConnectionAccessKey()
+    apikey = 'xoxkxkxx'
+
+    r = requests.request("GET", urljoin(c.BASE_URL, '/apiproxy/JobService.js'), params={'accesskey': apikey, 'method': 'GenerateAndPopulateAllSearches'})
+    print(r.status_code, r.text)
+
+    if r.status_code == 200:
+        return True
+    else:
+        return False
 
 
 @cli.command()
 @click.pass_context
-def GenerateAndPopulateAllSearchesForProduct(ctx):
-    template = '/apiproxy/JobService.js?accesskey={apikey}&method=GenerateAndPopulateAllSearchesForProduct&productGuid={product}'
+@click.option('--product', required=True, type=click.Choice(product_choices), prompt='Please enter the product GUID', default='DC', help='the product GUID')
+def regenone(ctx, product):
+    """
+    Regenerate and repopulate all searches in a single product
+    """
+    c = ctx.obj['client']
+
+    # apikey = c.GetConnectionAccessKey()
+    apikey = 'xoxkxkxx'
+
+    r = requests.request("GET", urljoin(c.BASE_URL, '/apiproxy/JobService.js'), params={'accesskey': apikey, 'method': 'GenerateAndPopulateAllSearchesForProduct', 'productGuid': product})
+    print(r.status_code, r.text)
+
+    if r.status_code == 200:
+        return True
+    else:
+        return False
 
 
 @cli.command()
 @click.pass_context
-def PopulateAllSearches(ctx):
-    template = '/apiproxy/JobService.js?accesskey={apikey}&method=PopulateAllSearches'
+def repopall(ctx):
+    """
+    Repopulate all searches in all products
+    """
+    c = ctx.obj['client']
+
+    # apikey = c.GetConnectionAccessKey()
+    apikey = 'xoxkxkxx'
+
+    r = requests.request("GET", urljoin(c.BASE_URL, '/apiproxy/JobService.js'), params={'accesskey': apikey, 'method': 'PopulateAllSearches'})
+    print(r.status_code, r.text)
+
+    if r.status_code == 200:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     cli(obj={})
