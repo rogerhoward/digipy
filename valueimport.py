1f987601362d2cddca11c35021c8a217c970731f
Roger Howard <rogerhoward@mac.com>
2018-10-19 13:20:55 -0400

    Marge treeimport and valueimport

--- valueimport.py
+++ valueimport.py
@@ -5,20 +5,24 @@ import click
 
 
 @click.command()
-@click.option('--file', required=True, type=click.Path(exists=True), help='Path to import file. See README for format.')
-@click.option('--field', required=True, type=int, help='ID of field to import into.')
 @click.option('--baseurl', required=True, help='Base URL of the Digizuite DAM Center.')
 @click.option('--username', required=True, default='System', help='API-capable Digizuite username; defaults to System.')
 @click.option('--password', required=True, help='Password for the provided username.')
-def main(file, field, baseurl, username, password):
+@click.option('--field', required=True, type=int, help='ID of field to import into.')
+@click.option('--type', required=True, type=click.Choice(['combo', 'tree']), help='Type of field to import into.')
+@click.option('--file', required=True, type=click.Path(exists=True), help='Path to import file. See README for format.')
+def main(baseurl, username, password, field, field_type, file):
     """A Digizuite API client which imports a value list into a controlled field."""
 
     # Create a Client instance
     dzClient = dz.Client(baseurl, username, password)
 
-    # Ask the Client to import the terms file
-    dzClient.importValuesToField(file, field)
+    if field_type == 'combo':
+        # Ask the Client to import a flat list of labels and values
+        dzClient.importFlatValues(file, field)
+    elif field_type == 'tree':
+        # Ask the Client to import a hierarchical list of labels
+        dzClient.importTreeValues(file, field)
 
 if __name__ == '__main__':
     main()
-
