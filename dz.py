93808f7a98faecb7b75d573ccd68b60c535bcd1f
Roger Howard <rogerhoward@mac.com>
2018-10-19 08:47:29 -0400

    add importValuesToField

--- dz.py
+++ dz.py
@@ -44,6 +44,32 @@ class Client(object):
                 lastLevel = level
 
 
+    def importValuesToField(self, file, fieldID):
+        login = self.GetConnectionAccessKey()
+        if login is False:
+            return False
+
+        with open(file) as values:
+            for line in values:
+                # level = node.rstrip().count('\t') + 1
+                # term = node.strip()
+                # print(term, level)
+
+                # if level > lastLevel:
+                #     # The last node processed is this node's parent, 
+                #     # add it to the stack
+                #     parentNodes.append(latestNode)
+                # elif level < lastLevel:
+                #     # Pop one or more parent nodes off the stack
+                #     # to match the current depth
+                #     [parentNodes.pop() for _ in range(lastLevel - level)]
+
+                # Create a new term
+                self.addValue(fieldID, term, value, accesskey=login)
+
+
+
+
     def GetConnectionAccessKey(self):
         url = urljoin(self.BASE_URL, 'dmm3bwsv3/ConnectService.js')
         method ='LogOn'
@@ -56,6 +82,44 @@ class Client(object):
             return res['items'][0]['accessKey']
         else:
             return False
+
+
+    def addValue(self, field, term, accesskey=None):
+        url = urljoin(self.BASE_URL, 'apiproxy/BatchUpdateService.js')
+
+        # If caller didn't pass in an access key, try to get one
+        if accesskey is None:
+            accesskey = self.GetConnectionAccessKey()
+            if accesskey is False:
+                return False
+
+        # Read in basic structure of "values" payload
+        with open('templates/values.json') as json_file:
+            payload_values = json.load(json_file)
+
+        # Insert custom values into "values" payload
+        payload_values[0]['Values'].append({"FieldId": "item_metafield_labelid", "Type": 3, "Values": field})
+        payload_values[0]['Values'].append({"FieldId": "prevref", "Type": 3, "Values": parent})
+        payload_values[0]['Values'].append({"FieldId": "treevalue", "Type": 1, "Values": [term]})
+        payload_values[0]['Values'].append({"FieldId": "optionvalue", "Type": 1, "Values": [str(uuid.uuid4())]})
+
+        # Read in XML contents for "updateXML" payload
+        with open("templates/update.xml") as xml_file:  
+            payload_xml = xml_file.read()
+
+        # Create requests payload dictionary
+        payload = {'updateXML': payload_xml, 'values': json.dumps(payload_values)}
+
+        # Make POST request, passing accesskey as a query param and other payload as form encoded
+        r = requests.request("POST", url, data=payload, params={'accesskey':accesskey})
+        res = r.json()
+
+        # Return creeated term's ID if successful
+        if res['success']:
+            return res['items'][0]['BaseId']
+        else:
+            return False
+
 
 
     def createTerm(self, field, term, parent=[0], accesskey=None):
